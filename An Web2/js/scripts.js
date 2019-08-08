4ad9af39b33eab22d77222874d1ec5ca9c944709
AnXBui <axb24@drexel.edu>
2019-08-08 01:21:40 -0400

    Animated Button Added

--- An Web2/js/scripts.js
+++ An Web2/js/scripts.js
@@ -1,7 +1,38 @@
-var $burger = $("#burger-icon");
+/* ==============================
+Burger Animation
+============================== */
+var $topLine = $('#burger-icon div:nth-child(1)');
+var $middleLine = $('#burger-icon div:nth-child(2)');
+var $bottomLine = $('#burger-icon div:nth-child(3)');
+var $burger= $('#burger-icon');
+var animationSpeed = 0.25;
+var ifClicked = false;
+
 var $nav = $('nav[data-nav]');
 var $navA = $('nav[data-nav] a');
 var isVisible = false;
+
+function burgerClick(){
+    console.log("clicks");
+    if(ifClicked === false){
+        TweenMax.to($topLine,animationSpeed,{rotation:45, y:10});
+        TweenMax.to($middleLine,animationSpeed,{alpha:0, scaleX:0});
+        TweenMax.to($bottomLine,animationSpeed,{rotation:-45, y:-10});
+        ifClicked = true;
+    }else{
+        TweenMax.to($topLine,animationSpeed,{rotation:0, y:0});
+        TweenMax.to($middleLine,animationSpeed,{alpha:1, scaleX:1});
+        TweenMax.to($bottomLine,animationSpeed,{rotation:0, y:0});
+        ifClicked = false;
+    }
+}
+
+$burger.on("click", burgerClick);
+$navA.on("click", burgerClick);
+/* ==============================
+Burger Animation End
+============================== */
+
 
 function mouseClick(){
 
